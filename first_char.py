bc35a82d7957e5e4332bfb519624359fb574a8fa
TBalint19 <toth910719balint@gmail.com>
2016-09-16 13:44:24 +0200

    boxes moves

--- first_char.py
+++ first_char.py
@@ -7,14 +7,18 @@ class Char:
         self.img = pygame.image.load(filename).convert()
         self.x = 20
         self.y = 20
+        self.moves = []
 
 
     def show_img(self, parent):
         parent.blit(self.img, (self.x, self.y))
 
-    def move(self):
-        self.x += 5
-# if lenyomtad:
-#     while not felemeled:
-#         menjen balra
-#             bla(dif 0 1)
+    def move(self, direction):
+        if direction == 'up':
+            self.y -= 10
+        if direction == 'right':
+            self.x += 10
+        if direction == 'left':
+            self.x -= 10
+        if direction == 'down':
+            self.y += 10
--- main.py
+++ main.py
@@ -22,7 +22,30 @@ while not game_over:
         if event.type == pygame.QUIT:
             game_over = True
         if event.type == pygame.KEYDOWN:
-            little_cube.move()
+            if event.key == pygame.K_RIGHT:
+                little_cube.move("right")
+                little_cube.moves.append("right")
+            if event.key == pygame.K_UP:
+                little_cube.move("up")
+                little_cube.moves.append("up")
+            if event.key == pygame.K_LEFT:
+                little_cube.move("left")
+                little_cube.moves.append("left")
+            if event.key == pygame.K_DOWN:
+                little_cube.move("down")
+                little_cube.moves.append("down")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                little_cube.moves.remove("right")
+            if event.key == pygame.K_UP:
+                little_cube.moves.remove("up")
+            if event.key == pygame.K_LEFT:
+                little_cube.moves.remove("left")
+            if event.key == pygame.K_DOWN:
+                little_cube.moves.remove("down")
+
+    for direction in little_cube.moves:
+        little_cube.move(direction)
 
 
     display.blit(background_image, [0, 0])
