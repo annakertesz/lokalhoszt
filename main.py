122b24734a7d5a8c451c4e85b82dec58a3f6e8be
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2016-09-16 19:25:56 +0200

    player 2 hits with enter

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ while not game_over:
             # punch
             elif event.key == pygame.K_SPACE:
                 char_2.punch()
-            elif event.key == pygame.K_0:
+            elif event.key == pygame.K_ENTER:
                 char_1.punch()
         # -------------------------
         elif event.type == pygame.KEYUP:
