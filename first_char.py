386a5b015ec02ad72f182cc1cffdd2feef654386
Csyk <balipaulcsik@gmail.com>
2016-09-16 15:11:43 +0200

    Char class skeleton

--- first_char.py
+++ first_char.py
@@ -3,11 +3,14 @@ import pygame
 class Char:
 
 
-    def __init__ (self, filename):
+    def __init__ (self, filename, direction, movestages, rectbox, life=100):
         self.img = pygame.image.load(filename).convert()
         self.x = 20
         self.y = 20
-        self.moves = []
+        self.life = life
+        self.direction = direction
+        self.movestages = movestages
+        self.rectbox = rectbox
 
 
     def show_img(self, parent):
@@ -22,3 +25,24 @@ class Char:
             self.x -= 10
         if direction == 'down':
             self.y += 10
+
+    def move(self):
+        pass
+
+    def jump(self):
+        pass
+
+    def crouch(self):
+        pass
+
+    def punch(self):
+        pass
+
+    def block(self):
+        pass
+
+    def kick(self):
+        pass
+
+    def head(self):
+        pass
